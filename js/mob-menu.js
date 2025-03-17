d0791a7ef625532fdc9dbadc3756b37ef5a541ca
anastasiia-kushch <anastasiia.kushch02@gmail.com>
2025-03-17 22:21:50 +0100

    add addaptive to header, modal

--- js/mob-menu.js
+++ js/mob-menu.js
@@ -1,6 +1,28 @@
-const openButton = document.getElementById('open-mob-menu');
+const openMobBtn = document.getElementById('open-mob-menu');
+const closeMobBtn = document.getElementById('close-mob-menu');
 const mobileMenu = document.getElementById('mobile-menu');
 
-openButton.addEventListener("click", () => {
-    mobileMenu.classList.add('menu-open')
+const openModalBtn = document.getElementById('open-modal');
+const closeModalBtn = document.getElementById('close-modal');
+const modalWindow = document.getElementById('modal-window');
+
+openMobBtn.addEventListener("click", () => {
+  mobileMenu.classList.add('menu-open')
+  document.body.classList.add('no-scroll');
 })
+
+closeMobBtn.addEventListener('click', () => {
+  mobileMenu.classList.remove('menu-open');
+  document.body.classList.remove('no-scroll');
+});
+
+
+openModalBtn.addEventListener('click', () => {
+  modalWindow.classList.add('is-open');
+  document.body.classList.add('no-scroll');
+});
+
+closeModalBtn.addEventListener('click', () => {
+  modalWindow.classList.remove('is-open');
+  document.body.classList.remove('no-scroll');
+});
